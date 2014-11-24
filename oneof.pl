d41563656459774ed0f5cf1549cd38d100c1452c
AlessandroBregoli <alessandroxciv@gmail.com>
2014-11-24 13:52:16 +0100

    aggiustato oneof

--- oneof.pl
+++ oneof.pl
@@ -1,2 +1,2 @@
-nfa_compile_token(FA_ID, InState, OutState, [oneof | Args], falso) :-
-	nfa_compile_token(FA_ID, InState, OutState, [alt | Args], falso).
+nfa_compile_token(FA_ID, InState, OutState, [oneof | Args], NEGATO) :-
+	nfa_compile_token(FA_ID, InState, OutState, [alt | Args], NEGATO).
